06197f7f59729f8f45e3a9e97b49e0b35a37c3de
Nqobile <nqobi.khuz@gmail.com>
2023-03-19 08:21:33 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 int main(0)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
-
-	puts(Mystr);
-
+	puts(\"Programming is like building a multilingual puzzle)
 	return (0);
 }

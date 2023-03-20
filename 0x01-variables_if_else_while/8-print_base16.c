8868ad697cd969c77323cf51382020f84a073831
Nqobile <nqobi.khuz@gmail.com>
2023-03-20 11:58:27 -0700

    hexi

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,15 @@
 int main(void)
 {
 	int n;
+	char l;
 
-	for (n = 0; n <= 16; n++)
+	for (n = 0; n <= 10; n++)
 {
 	putchar(n + '0');
+}
+	for (l = 'a'; l <= 'f'; l++)
+{
+	putchar(l);
 }
 	putchar('\n');
 	return (0);

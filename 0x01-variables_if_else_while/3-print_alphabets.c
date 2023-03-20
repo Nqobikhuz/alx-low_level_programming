46a8112a05597cbf0bceffd94a02c9a37bd3a5f6
Nqobile <nqobi.khuz@gmail.com>
2023-03-20 01:21:28 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description - print the alphabet in lower and higher case
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char lc;
+	char uc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	for (uc = 'A'; uc <= 'Z'; uc++)
+	{
+		putchar(uc);
+	}
+	return (0);
+}

fc69e31f47f564bb63e2bafaef1d0c8457d8f378
Nqobile <nqobi.khuz@gmail.com>
2023-03-21 16:20:22 -0700

    prints every second of the day

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - is a funstion that
+ * prints the last digit of an int
+ *
+ * @i: input numbers
+ *
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int i)
+{
+	int l;
+
+	l = n % 10
+
+	if (i > 0)
+	{
+		return (l);
+	}
+	else
+	{
+		return (l * -1);
+	}
+}

69f428fed6dd42f258435f315c7098be12846f9e
Nqobile <nqobi.khuz@gmail.com>
2023-03-23 10:08:45 -0700

    checking uppercase numbers

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characters.
+ *
+ * @c: Input values
+ *
+ * Return: 1 if c is uppercase
+ * otherwise it will return 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}

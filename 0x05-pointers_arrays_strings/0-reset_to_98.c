15110a5e14b08aa40043ed361532f7ddc6272878
Nqobile <nqobi.khuz@gmail.com>
2023-03-27 12:17:50 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as a
+ * parameter and updates the vlaue it points to 98
+ * @n: an integer
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

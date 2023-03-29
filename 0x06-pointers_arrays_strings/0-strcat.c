5475cdc12fb78c82cbf6c30255b2eb556d93a104
Nqobile <nqobi.khuz@gmail.com>
2023-03-29 11:54:15 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - will concatenate two scrings
+ * @dest: first string
+ * @src: string to be appended to dest
+ * Return: characters
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int destlength = 0;
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		destlength++;
+
+	for (i = 0; src[i]; i++)
+		dest[destlength + i] = src[i];
+
+	return (dest);
+}

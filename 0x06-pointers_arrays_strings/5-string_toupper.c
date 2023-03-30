fce67184c7bf7bc454c034bfb9f65d62c6922cf2
Nqobile <nqobi.khuz@gmail.com>
2023-03-30 14:20:30 -0700

    string_toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,12 +13,9 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] >= 'z')
-		{
+		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] = str[i] - 32;
-		}
-		i++;
 	}
-	return (0);
+	return (str);
 }
 

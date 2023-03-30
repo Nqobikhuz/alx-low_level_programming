d27aa85ef1586b14725835ab2fc1b5d9c569b56a
Nqobile <nqobi.khuz@gmail.com>
2023-03-30 10:49:31 -0700

    s

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,10 +11,13 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int destlength = 0;
+	int srclength = 0;
 	int i;
 
 	for (i = 0; dest[i] != '\0'; i++)
 		destlength++;
+	for (i = 0; src[i] != '\0'; i++)
+		srclength++;
 
 	for (i = 0; i < n; i++)
 		dest[destlength + i] = src[i];

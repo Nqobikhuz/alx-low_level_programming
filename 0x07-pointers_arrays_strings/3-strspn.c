3b6c742d24382304a5f41d472345c2fe839074af
Nqobile <nqobi.khuz@gmail.com>
2023-04-03 13:29:06 -0700

    strstr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,17 +17,16 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i;
 	unsigned int j;
-	unsigned int ans;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-		if (s[i] == accept[j])
+		if (s[i] != '\0')
 		{
-			ans++;
+			return (i);
 		}
 		}
 	}
-	return (0);
+	return (i);
 }

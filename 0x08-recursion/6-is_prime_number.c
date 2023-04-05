f35e5fa81ff76e19b78c6edebabb2c903fe8f8d8
Nqobile <nqobi.khuz@gmail.com>
2023-04-05 07:53:30 -0700

    factorial

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * is_prime_number - identifies if an integer is a prime numer
+ *
+ * @n: input integers
+ *
+ * Return: 1 if the integer is a prime numer
+ * otherwise 0
+ */
+
+int is_prime_number(int n)
+{
+

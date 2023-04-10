04b22ed18f179d4163f0fc34fb0cdc6801661488
Nqobile <nqobi.khuz@gmail.com>
2023-04-10 09:58:00 -0700

    prints name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name
+ * @argc: contains the number of arguments passed to the program
+ * @argv: is a one-dimensional array of strings
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for( i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}

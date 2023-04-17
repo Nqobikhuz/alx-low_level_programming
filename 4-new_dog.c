dd83bee891b6d9397492d6609e8e6eecc0f8063a
Nqobile <nqobi.khuz@gmail.com>
2023-04-17 12:46:17 -0700

    free dog

---
+++ 4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates attributes for a new dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog's owner
+ *
+ * Return: 0
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+

778fe1dc55928e191784b5a98d4db2400953e687
Nqobile <nqobi.khuz@gmail.com>
2023-04-19 08:31:26 -0700

    1-array_iterator.c function created

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searches for an integer
+ *
+ * @array: array of values to be checked
+ * @size: number of elements in array
+ * @cmp: is a pointer to the function to be
+ * used to compare values
+ *
+ * Return: the index of the first element for
+ * which the cmp function does not return 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array != NULL && cmp != NULL && size < 0)
+	{
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+
+	}
+	if (size <= 0)
+		return (-1);
+
+	return (0);
+}

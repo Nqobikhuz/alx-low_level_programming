4d218a9f50f5c13424beb4f8992436e70198e261
Nqobile <nqobi.khuz@gmail.com>
2023-04-19 11:33:43 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,18 +16,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (array != NULL && cmp != NULL && size < 0)
+	if (array != NULL && cmp != NULL && size > 0)
 	{
 		while (i < size)
 		{
 			if (cmp(array[i]))
-				return (i);
+			return (i);
 			i++;
 		}
-
 	}
-	if (size <= 0)
 		return (-1);
-
-	return (0);
 }

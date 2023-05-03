f0228a1313afe69d41b4f13524997195da1ddc86
Nqobile <nqobi.khuz@gmail.com>
2023-05-03 12:46:08 -0700

    changes

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,16 +10,16 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (head == NULL)
+	if (*head == NULL)
 	{
 		return;
 	}
 
-	while (head)
+	while (*head)
 	{
-		temp = head->next;
-		temp = head;
+		temp = (*head)->next;
+		temp = *head;
 		free(temp);
 	}
-	head = NULL;
+	*head = NULL;
 }

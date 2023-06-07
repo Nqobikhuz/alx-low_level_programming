24667cfbe8d3f55974fccde8d8d156ac858d5f61
Nqobile <nqobi.khuz@gmail.com>
2023-06-07 14:43:58 -0700

    betty compliance

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,16 +4,26 @@
  * _sqrt_recursion - returns the natural
  * square root of a number
  *
- * @n: natural numbers
+ * @n: input numbers
  *
  * Return: natural square root
  */
-int _square(int n, int i);
 
 int _sqrt_recursion(int n)
 {
 	return (_square(n, 1));
 }
+
+/**
+ * _square - programe used to check if a number
+ * is natural
+ *
+ * @n: input number to be checked
+ * @i:a variable that represents the current
+ * value being checked for its square
+ *
+ * Return: natural square root of a number
+ */
 
 int _square(int n, int i)
 {
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -18,7 +18,7 @@ int is_prime_number(int n)
 		return (0);
 	}
 
-	for (i = 2; i <= n/2; i++)
+	for (i = 2; i <= n / 2; i++)
 	{
 		if (n % i == 0)
 			return (0);

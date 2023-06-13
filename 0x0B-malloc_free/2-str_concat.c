cdec888f00404e0d5e7bcd1533b9465f327c2068
Nqobile <nqobi.khuz@gmail.com>
2023-06-13 03:52:37 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,34 +15,40 @@ char *str_concat(char *s1, char *s2)
 {
 	int s1length = 0;
 	int s2length = 0;
-	int i, k;
-	char *j;
+	int i, j;
+	char *concat;
 
 	if (s1 == NULL)
 		s1 = "\0";
+
 	if (s2 == NULL)
 		s2 = "\0";
 
-	for (i = 0; s1[i] != '\0'; i++)
-		s1length++;
-
-	for (i = 0; s2[i] != '\0'; i++)
-		s2length++;
-
-	j = malloc(sizeof(char) * (s1length + s2length) + 1);
-
-	if (j == NULL)
-		return (NULL);
-
-	for (k = 0; k < s1length + s2length; k++)
+	while (s1length != '\0')
 	{
-		if (k <= i)
-			j[k] = s1[k];
-
-		else
-			j[k] = s2[i - s2length];
+		s1length++;
 	}
 
-	j[k] = '\0';
-	return (j);
+	while (s2length != '\0')
+	{
+		s2length++;
+	}
+
+	concat = malloc(sizeof(char) * (s1length + s2length) + 1);
+
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < s1length; i++)
+	{
+		s1[i] = concat[i];
+	}
+
+	for (j = 0; j < s2length; j++)
+	{
+		s2[j] = concat[i = j];
+	}
+
+	concat[i + j] = '\0';
+	return (concat);
 }

351fcfc1d3d9ee1e7e2d96850ea0375f78f37043
Nqobile <nqobi.khuz@gmail.com>
2023-06-15 11:22:55 -0700

    all

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,12 +24,12 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "\0";
 
-	while (s1length != '\0')
+	while (s1[s1length] != '\0')
 	{
 		s1length++;
 	}
 
-	while (s2length != '\0')
+	while (s2[s2length] != '\0')
 	{
 		s2length++;
 	}
@@ -39,14 +39,14 @@ char *str_concat(char *s1, char *s2)
 	if (concat == NULL)
 		return (NULL);
 
-	for (i = 0; i < s1length; i++)
+	for (i = 0; i < s1length + s2length; i++)
 	{
 		s1[i] = concat[i];
 	}
 
 	for (j = 0; j < s2length; j++)
 	{
-		s2[j] = concat[i = j];
+		s2[j] = concat[i - j];
 	}
 
 	concat[i + j] = '\0';
